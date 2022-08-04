877c107bf26c4e422a28ae71b1ff47d68701fefe
Jeisil Hernandez <107511517+GoatNinja@users.noreply.github.com>
2022-08-04 14:58:15 -0500

    Create Day 1.py

---
+++ Day 1.py
@@ -0,0 +1,8 @@
+# Day 1
+
+# get users city
+city = input("What is the city you were born in? ")
+# get users pet name 
+pet = input("What is the name of your pet? ")
+# Combine city and name 
+print(city + pet,)
